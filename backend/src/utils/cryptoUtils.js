4a571614751e063508eadede1b7c5fec0301507c
Bica Marius Adrian <46315673+BicaMarius@users.noreply.github.com>
2025-06-04 21:33:14 +0300

    Remove unused code

--- backend/src/utils/cryptoUtils.js
+++
@@ -1,3 +0,0 @@
-module.exports = {};
-// Acest fișier poate conține funcții utilitare suplimentare pentru operații criptografice.
-// Deocamdată, logica de criptare este gestionată în encryptionService.js.
